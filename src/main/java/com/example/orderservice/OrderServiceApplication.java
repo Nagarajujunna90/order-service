1ed927d8861bdd6d6d4000a91392f6cceb0e8088
njunna <nagarajujunna90@gmail.com>
2023-07-04 19:27:37 +0530

    changes

--- src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.orderservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "order-service"))
 @SpringBootApplication
 //@EnableEurekaClient
 public class OrderServiceApplication {

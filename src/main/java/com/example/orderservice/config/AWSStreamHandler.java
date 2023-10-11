59299dc4e8746118f6dcf097cf2bb9c15c03c149
njunna <nagarajujunna90@gmail.com>
2023-10-11 14:07:14 +0530

    updated

---
+++ src/main/java/com/example/orderservice/config/AWSStreamHandler.java
@@ -0,0 +1,38 @@
+package com.example.orderservice.config;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.example.orderservice.OrderServiceApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class AWSStreamHandler implements RequestStreamHandler {
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+                    .defaultProxy()
+                    .asyncInit()
+                    .springBootApplication(OrderServiceApplication.class)
+                    .buildAndInitialize();
+        } catch (ContainerInitializationException e) {
+            // if we fail here, we re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
